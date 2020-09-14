0831d9841fac967d3bb38ea779356e2476ff6188
Manuel Florez <manuelflorezw@gmail.com>
2020-09-14 00:51:33 -0500

    ejemplo de data(), atributo de clase

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ name }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -40,6 +41,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
+  name = 'Manuel';
   @Prop() private msg!: string;
 }
 </script>
